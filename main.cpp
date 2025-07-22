64fa7accd8c1e2c4031d7cb2e9c482be4948859b
Nemexia <amirhossein.rahmani55@gmail.com>
2025-07-22 17:13:49 +0330

    Add Calcer function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 
+int Calcer(const int cycles)
+{
+    int sum{};
+    for(int i{}; i<cycles; ++i)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
 int main()
 {
     std::cout<<"Hello World!\n";
+
+    std::cout<<Calcer(17)<<'\n';
+
     return 0;
 }
